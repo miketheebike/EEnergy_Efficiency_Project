bad41a21d7375033f80831a7dec08a5b36698f6d
laragazzadelsole <sara.gironi97@gmail.com>
2024-09-11 11:52:52 +0200

    Margherita and Mike feedback integration

--- changing_components.py
+++ changing_components.py
@@ -13,7 +13,8 @@ from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 from fixed_components import *
-import altair as alt
+
+import plotly.graph_objs as go
 
 def initialize_session_state():
     if 'key' not in st.session_state:
@@ -36,15 +37,9 @@ def initialize_session_state():
             'Minimum Effect Size Q6': [],
             'Minimum Effect Size Q7': [],
             'Minimum Effect Size Q8': [],
-            'Minimum Effect Size Q9': [],
+            #'Minimum Effect Size Q9': [],
             'Cost-Benefit Ratio': [],
             'Risk Aversion': []
-            #'RCT Q1': [],
-            #'RCT Q2': [],
-            #'RCT Q3': [],
-            #'RCT Q4': [],
-            #'RCT Q5': [],
-            #'RCT Q6': []
             }
     
 def safe_var(key):
@@ -56,7 +51,6 @@ def survey_title_subtitle(header_config):
     st.write(header_config['survey_description'])
 
 def create_question(jsonfile_name):
-
     minor_value = str(jsonfile_name['minor_value'])
     min_value = jsonfile_name['min_value_graph']
     max_value = jsonfile_name['max_value_graph']
@@ -64,18 +58,18 @@ def create_question(jsonfile_name):
     major_value = str(jsonfile_name['major_value'])
 
     # Create a list of ranges based on the provided values
-    x_axis = [minor_value] + [f"{round(i, 1)} - {round((i + interval - 0.01), 2)}" for i in np.arange(min_value, max_value, interval)] + [major_value]
+    x_axis = [minor_value] + [f"{round(i, 1)}% to {round((i + interval - 0.01), 2)}%" for i in np.arange(min_value, max_value, interval)] + [major_value]
 
     # TODO find a way to remove it
     if jsonfile_name['min_value_graph'] == -1:
-        x_axis.insert(6, 0)
-        x_axis[1] = '-0.99 - -0.81'
-        x_axis[7] = '0.01 - 0.19'
+        x_axis.insert(6, "0%")
+        x_axis[1] = '-0.99% to -0.81%'
+        x_axis[7] = '0.01% to 0.19%'
     elif jsonfile_name['min_value_graph'] == -15:
-        x_axis.insert(4, 0)
-        x_axis[5] = '0.01 - 4.99'
+        x_axis.insert(4, "0%")
+        x_axis[5] = '0.01% to 4.99%'
     elif jsonfile_name['min_value_graph'] == 0:    
-        x_axis[1] = '0.01 - 4.99'
+        x_axis[1] = '0.01% to 4.99%'
 
     y_axis = np.zeros(len(x_axis))
 
@@ -89,27 +83,67 @@ def create_question(jsonfile_name):
         table, plot = st.columns([0.4, 0.6], gap="large")
         with table:
             bins_grid = st.data_editor(data, key= jsonfile_name['key'], hide_index=True, use_container_width=True, disabled=[jsonfile_name['column_1']])
-
             percentage_difference = 100 - sum(bins_grid[jsonfile_name['column_2']])
 
             # Display the counter
             if percentage_difference > 0:
-                st.write(f"**You still have to allocate {percentage_difference} percent probability.**")
+                missing_prob = f'<b style="font-family:sans-serif; color:Green; font-size: 20px; ">You still have to allocate {percentage_difference}% probability.</b>'
+                st.markdown(missing_prob, unsafe_allow_html=True)
+                
             elif percentage_difference == 0:
-                st.write(f'**You have allocated all probabilities!**')
+                total_prob = f'<b style="font-family:sans-serif; color:Green; font-size: 20px; ">You have allocated all probabilities!</b>'
+                st.markdown(total_prob, unsafe_allow_html=True)
             else:
-                st.write(f'**:red[You have inserted {abs(percentage_difference)} percent more, please review your percentage distribution.]**')           
-                  
+                exceeding_prob = f'<b style="font-family:sans-serif; color:Red; font-size: 20px; ">You have inserted {abs(percentage_difference)}% more, please review your percentage distribution.</b>'
+                st.markdown(exceeding_prob, unsafe_allow_html=True)
+                      
         with plot:
-            # TODO check performance difference with matplotlib
-            chart = alt.Chart(bins_grid).mark_bar().encode(
-                x=alt.X(jsonfile_name['column_1'], sort=None),
-                y=jsonfile_name['column_2']
-            )
+            # Extract the updated values from the second column
+            updated_values = bins_grid[jsonfile_name['column_2']]
 
-            st.altair_chart(chart, use_container_width=True)
-    
-    return pd.DataFrame(bins_grid), percentage_difference,  len(bins_grid)
+            # Plot the updated values as a bar plot
+            fig = go.Figure()
+            fig.add_trace(go.Bar(
+                x=bins_grid[jsonfile_name['column_1']], 
+                y=updated_values, 
+                marker_color='rgba(50, 205, 50, 0.9)',  # A nice bright green
+                marker_line_color='rgba(0, 128, 0, 1.0)',  # Dark green outline for contrast
+                marker_line_width=2,  # Width of the bar outline
+                text=[f"{p}" for p in bins_grid[jsonfile_name['column_2']]],  # Adding percentage labels to bars
+                textposition='auto',
+                name='Probability'
+            ))
+
+            fig.update_layout(
+                title={
+                    'text': "Probability distribution",
+                    'y':0.9,
+                    'x':0.5,
+                    'xanchor': 'center',
+                    'yanchor': 'top'
+                },
+                xaxis_title="Expectation Range",
+                yaxis_title="Probability (%)",
+                yaxis=dict(
+                    range=[0, 100], 
+                    gridcolor='rgba(255, 255, 255, 0.2)',  # Light grid on dark background
+                    showline=True,
+                    linewidth=2,
+                    linecolor='white',
+                    mirror=True
+                ),
+                xaxis=dict(
+                    tickangle=-45,
+                    showline=True,
+                    linewidth=2,
+                    linecolor='white',
+                    mirror=True
+                ),
+                font=dict(color='white'),  # White font color for readability
+            )
+            st.plotly_chart(fig)
+
+    return pd.DataFrame(bins_grid), percentage_difference, len(bins_grid)
 
 def effect_size_question(jsonfile_name):
     col1, _ = st.columns(2)
@@ -117,37 +151,9 @@ def effect_size_question(jsonfile_name):
         st.markdown(jsonfile_name['effect_size'])
         st.text_input("Please insert a number or write 'I don't know'.", key = jsonfile_name['num_input_question'])
 
-def RCT_questions():
-    st.subheader('Questions on RCTs Evaluation')
-    st.write('We would like to know your opinion regarding RCTs programs.')
-    st.write('1. After my experience in being involved in this project, I am:')
-    st.radio('Choose one of the following options:', ['More interested in using RCTs for evaluation of other government projects', 'Just as interested in using RCTs for evaluation of other government projects as I was before starting this one', 'Less interested in using RCTs for evaluation of other government projects'], key ='RCT_question1')
-    st.write('2. We would like you to compare your experiences on this project that we are evaluating through an experiment to similar government projects you have worked on that have not had such an evaluation. Can you please compare this project to similar projects without an experimental evaluation in terms of:')
-    st.write('- Design of the intervention')
-    st.radio('Choose one of the following options:', ['The RCT improved the design of the intervention relative to projects without an RCT', 'The RCT did not change the design', 'The RCT led the intervention to be designed less well than projects without an RCT'], key='RCT_question2')
-    st.write('- Speed of Implementation')
-    st.radio('Choose one of the following options:', ['The RCT sped up implementation of the project', 'The RCT did not change the speed', 'The RCT slowed down the speed of implementation'], key='RCT_question3')
-    st.write('- Trustiworthiness of program impacts')
-    st.radio('Choose one of the following options:', ['I will trust estimates of the programs impacts from this RCT more than of other programs that use our standard M&E', "I will trust estimates of this program's impacts equally as much as other programs that use our standard M&E", "I will trust estimates of this program's impacts from the RCT less than those of other programs that use our standard M&E"], key='RCT_question4')
-    st.write('- Do you think that thanks to the RCT you reached new beneficiaries? Do you think that it helped you disburse more funds than you originally planned?')
-    st.text_input('Please, write about your experience (max 500 characters).',max_chars=500, key = 'RCT_question5')
-    st.write('- Do you think allocating grants randomly amongst equally eligible potential beneficiaries is ethical? Did you think so before engaging in the RCT?')
-    st.text_input('Please, write about your experience (max 500 characters).', max_chars=500, key = 'RCT_question6')
-
 def add_submission(updated_bins_question_1_df, updated_bins_question_2_df, updated_bins_question_3_df, updated_bins_question_4_df, updated_bins_question_5_df, updated_bins_question_6_df, updated_bins_question_7_df, updated_bins_question_8_df, updated_bins_question_9_df):
 
     updated_bins_list = [updated_bins_question_1_df, updated_bins_question_2_df, updated_bins_question_3_df, updated_bins_question_4_df, updated_bins_question_5_df, updated_bins_question_6_df, updated_bins_question_7_df, updated_bins_question_8_df, updated_bins_question_9_df]
-    # Extracting the first row of each transposed dataframe as column names
-    #for i, transposed_df in enumerate(transposed_bins_list):
-    #    transposed_df.columns = column_names_list[i]
-
-    # Removing the first row (used as column names) from each dataframe
-    #transposed_bins_list = [transposed_df.iloc[1:] for transposed_df in transposed_bins_list]
-
-    # Adding prefix to column names of each dataframe
-    #for i, transposed_df in enumerate(transposed_bins_list):
-    #    prefix = f'Q{i + 1}  '
-    #    transposed_df.columns =  [f'{prefix}{col}' for col in transposed_df.columns]
 
     def restructure_df(df, i):
         transposed_df = df.transpose()
@@ -180,16 +186,10 @@ def add_submission(updated_bins_question_1_df, updated_bins_question_2_df, updat
     MIN_EFF_SIZE_Q6 = 'Minimum Effect Size Q6'
     MIN_EFF_SIZE_Q7 = 'Minimum Effect Size Q7'
     MIN_EFF_SIZE_Q8 = 'Minimum Effect Size Q8'
-    MIN_EFF_SIZE_Q9 = 'Minimum Effect Size Q9'
+    #MIN_EFF_SIZE_Q9 = 'Minimum Effect Size Q9'
     #MIN_EFF_SIZE_Q10 = 'Minimum Effect Size Q10'
     COST_BENEFIT_RATIO = 'Cost-Benefit Ratio'
     RISK_AVERSION = 'Risk Aversion'
-    #RCT_Q1 = 'RCT Q1'
-    #RCT_Q2 = 'RCT Q2'
-    #RCT_Q3 = 'RCT Q3'
-    #RCT_Q4 = 'RCT Q4'
-    #RCT_Q5 = 'RCT Q5'
-    #RCT_Q6 = 'RCT Q6'
 
     data[USER_FULL_NAME].append(safe_var('user_full_name'))
     data[USER_POSITION].append(safe_var('user_position'))
@@ -203,26 +203,20 @@ def add_submission(updated_bins_question_1_df, updated_bins_question_2_df, updat
     data[MIN_EFF_SIZE_Q6].append(safe_var('num_input_question6'))
     data[MIN_EFF_SIZE_Q7].append(safe_var('num_input_question7'))
     data[MIN_EFF_SIZE_Q8].append(safe_var('num_input_question8'))
-    data[MIN_EFF_SIZE_Q9].append(safe_var('num_input_question9'))
+    #data[MIN_EFF_SIZE_Q9].append(safe_var('num_input_question9'))
     #data[MIN_EFF_SIZE_Q10].append(safe_var('num_input_question10'))
     data[COST_BENEFIT_RATIO].append(safe_var('cost_benefit'))
     data[RISK_AVERSION].append(safe_var('risk_aversion'))
-    #data[RCT_Q1].append(safe_var('RCT_question1'))
-    #data[RCT_Q2].append(safe_var('RCT_question2'))
-    #data[RCT_Q3].append(safe_var('RCT_question3'))
-    #data[RCT_Q4].append(safe_var('RCT_question4'))
-    #data[RCT_Q5].append(safe_var('RCT_question5'))
-    #data[RCT_Q6].append(safe_var('RCT_question6'))
 
     st.session_state['data'] = data
     
     session_state_df = pd.DataFrame(data)
+    
     personal_data_df = session_state_df.iloc[:, :4]
     min_eff_df = session_state_df.iloc[:, 4:]
 
     concatenated_df = pd.concat([personal_data_df, questions_df.set_index(personal_data_df.index), min_eff_df.set_index(personal_data_df.index)], axis=1)
-    
-    
+      
     st.session_state['submit'] = True
     
     #save data to google sheet
