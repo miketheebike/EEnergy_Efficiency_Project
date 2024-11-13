43dbbf2504ac9bcc4f704dbc9cd93188f1211736
Mike <137076652+miketheebike@users.noreply.github.com>
2024-11-13 14:52:19 +0000

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if st.session_state.page == 0:
     
         personal_information()
         
-        next_page()
+        st.button("Next", on_click=next_page)
     
 if st.session_state.page == 1:
     entrepreneur_firm_representative_question()
