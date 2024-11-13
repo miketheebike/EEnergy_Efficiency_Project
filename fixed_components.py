37a79b6e7a0f903bccc1e3e01b96df7e866cf4de
Mike <137076652+miketheebike@users.noreply.github.com>
2024-11-13 15:32:42 +0000

    Update fixed_components.py

--- fixed_components.py
+++ fixed_components.py
@@ -46,7 +46,7 @@ def personal_information():
         st.text_input("Please, enter your full name:", key = 'user_full_name')
         st.text_input("Please, enter your working title:", key = 'user_position')
         st.selectbox('Please, specify your professional category:', ('Policy implementer (EENergy consortium working package leaders)', 'Donor (European Commission)', 'Researcher', 'Sustainability Advisor', 'Entrepreneur/Firm Representative'), key="professional_category")
-        
+        st.button("Next Page", on_click=next_page)
 #         st.number_input(
 #     'Please, insert the years of experience you have working on energy efficiency:', 
 #     min_value=0.0, 
