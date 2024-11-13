a02614002ae684c5ac405f550574098085158a85
Mike <137076652+miketheebike@users.noreply.github.com>
2024-11-13 16:58:05 +0000

    Update fixed_components.py

--- fixed_components.py
+++ fixed_components.py
@@ -267,7 +267,7 @@ def instructions():
                 mirror=True
             ),
             font=dict(color='white'),    # White font color for readability
-        width = 500,# Adjust width here
+        width = 400,# Adjust width here
         height = 400
         )
         st.plotly_chart(fig)
