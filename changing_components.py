8fbac7812855620739d42155efd0c569f98f0a94
Mike <137076652+miketheebike@users.noreply.github.com>
2024-11-14 11:07:09 +0000

    Update changing_components.py

--- changing_components.py
+++ changing_components.py
@@ -144,7 +144,7 @@ def create_question(jsonfile_name):
             updated_values = bins_grid[jsonfile_name['column_2']]
 
             # Get rid of plot menu
-            config = {'displayModeBar': False}
+            config = {'displayModeBar': False, "dragmode": False}
                     
             # Plot the updated values as a bar plot
             fig = go.Figure()
