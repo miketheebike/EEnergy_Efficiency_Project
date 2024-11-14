68efe776ef71fefd83ba0848f6536e0aa12519bc
Mike <137076652+miketheebike@users.noreply.github.com>
2024-11-14 12:29:45 +0000

    Update changing_components.py

--- changing_components.py
+++ changing_components.py
@@ -104,7 +104,7 @@ def create_question(jsonfile_name):
         x_axis.insert(6, "0%")
         x_axis[1] = '-0.99% to -0.81%'
         x_axis[7] = '0.01% to 0.19%'
-    elif jsonfile_name['min_value_graph'] == -15:
+    elif jsonfile_name['min_value_graph'] == -10:
         x_axis.insert(4, "0%")
         x_axis[5] = '0.01% to 4.99%'
     elif jsonfile_name['min_value_graph'] == 0:    
